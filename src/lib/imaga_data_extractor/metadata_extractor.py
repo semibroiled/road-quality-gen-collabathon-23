d3aabb6f671dd4eb42268310c8fc3e1dd8c1dcd1
Fatma Shalaby <fatmaa.shalabyy@gmail.com>
2023-10-21 17:08:50 +0200

    LAST

--- src/lib/imaga_data_extractor/metadata_extractor.py
+++ src/lib/imaga_data_extractor/metadata_extractor.py
@@ -14,8 +14,7 @@ def extract_metadata(image_path):
         exif_data = img._getexif()
         if exif_data:
             for tag, value in exif_data.items():
-                tag_name = exifread.FIELD_TYPES[tag]
-                print(f"{tag_name} ({tag}): {value}")
+                print(f"{tag}: {value}")  # Print tag name and value directly
 
 if __name__ == "__main__":
     image_path = "/Users/fatmashalaby/Documents/road-quality-gen-collabathon-23/src/lib/imaga_data_extractor/images/62774941-A965-4DC8-8376-B88627E3FA33.jpeg"
